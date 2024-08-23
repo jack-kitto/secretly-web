7a72cb954a4ff573512ed074c89337e966bab8cc
Jack Kitto <jack.kitto@gmail.com>
2024-08-23 20:28:29 +1000

    feat: Hide command in the add envs section and rename accordion item data struct

--- cmd/web/walkthrough.go
+++ cmd/web/walkthrough.go
@@ -7,35 +7,43 @@ import (
 	"net/http"
 )
 
-func (item AccordionItemData) descriptionStyles() string {
+func (item WalkthroughItemData) descriptionStyles() string {
 	// if item.isOpen {
 	// 	return " max-h-60  "
 	// }
 	return " max-h-0"
 }
 
-func (item AccordionItemData) getIsOpen() string {
+func (item WalkthroughItemData) getHideCommand() string {
+	if item.hideCommand {
+		return "true"
+	}
+	return "false"
+}
+
+func (item WalkthroughItemData) getIsOpen() string {
 	if item.isOpen {
 		return "false"
 	}
 	return "true"
 }
 
-func (item AccordionItemData) chevronStyles() string {
+func (item WalkthroughItemData) chevronStyles() string {
 	if item.isOpen {
 		return "-rotate-90"
 	}
 	return ""
 }
 
-type AccordionItemData struct {
+type WalkthroughItemData struct {
 	title       string
 	description string
 	command     string
 	isOpen      bool
+	hideCommand bool
 }
 
-func (i *AccordionItemData) Print() {
+func (i *WalkthroughItemData) Print() {
 	if i == nil {
 		panic("Missing param i AccordionItemData")
 	}
@@ -46,7 +54,7 @@ func (i *AccordionItemData) Print() {
 	log.Print(string(res))
 }
 
-func (item *AccordionItemData) decodeParams(r *http.Request) {
+func (item *WalkthroughItemData) decodeParams(r *http.Request) {
 	if item == nil {
 		panic("invalid item given to decode into AccordionItemData")
 	}
@@ -54,46 +62,51 @@ func (item *AccordionItemData) decodeParams(r *http.Request) {
 	item.description = r.URL.Query().Get("description")
 	item.command = r.URL.Query().Get("command")
 	item.isOpen = r.URL.Query().Get("isOpen") == "true"
+	item.hideCommand = r.URL.Query().Get("hideCommand") == "true"
 }
 
-func (item AccordionItemData) encodeParams() string {
-	return fmt.Sprintf("title=%s&description=%s&isOpen=%s&command=%s",
+func (item WalkthroughItemData) encodeParams() string {
+	return fmt.Sprintf("/toggle-accordion?title=%s&description=%s&isOpen=%s&command=%s&hideCommand=%s",
 		item.title,
 		item.description,
 		item.getIsOpen(),
 		item.command,
+		item.getHideCommand(),
 	)
 }
 
-func installCLIItem() AccordionItemData {
-	return AccordionItemData{
+func installCLIItem() WalkthroughItemData {
+	return WalkthroughItemData{
 		title:       "Install the CLI",
 		description: "Power up your terminal! ⚡️ Get our CLI for lightning-fast secret management. One command away from secure coding bliss.",
 		command:     "go install github.com/jack-kitto/secretly-cli/v1.0.0",
+		hideCommand: false,
 		isOpen:      false,
 	}
 }
 
-func createProjectItem() AccordionItemData {
-	return AccordionItemData{
+func createProjectItem() WalkthroughItemData {
+	return WalkthroughItemData{
 		title:       "Create a project",
 		description: "Your secrets need a home 🏠 Set up a cozy project space. Name it, tag it, make it yours. Organisation made simple. Run this command from one of your projects",
 		command:     "secretly --token XXXX-YYYY --init",
 		isOpen:      false,
+		hideCommand: false,
 	}
 }
 
-func addEnvItem() AccordionItemData {
-	return AccordionItemData{
+func addEnvItem() WalkthroughItemData {
+	return WalkthroughItemData{
 		title:       "Add your envs",
 		description: "Shhh... your secrets are safe here 🤫 In Secretly, you can drag, drop, or paste your .env files anywhere on any page. We'll tuck them away securely in seconds, give it a shot!",
 		command:     "secretly --token XXXX-YYYY --init",
 		isOpen:      false,
+		hideCommand: true,
 	}
 }
 
 func ToggleAccordion(w http.ResponseWriter, r *http.Request) {
-	item := AccordionItemData{}
+	item := WalkthroughItemData{}
 	item.decodeParams(r)
 
 	component := AccordionItem(item)
